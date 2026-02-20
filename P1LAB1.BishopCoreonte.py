2e5ab541829e44360c092d9b274e9c972efa5420
bishopc0383 <bishopc0383@student.faytechcc.edu>
2026-02-20 12:38:20 -0500

    first python assignment
    taking input from user and printing output

---
+++ P1LAB1.BishopCoreonte.py
@@ -0,0 +1,8 @@
+# Coreonte Bishop
+# P1LAB1.BishopCoreonte.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name + "!" , "Welcome to CTI-110" + "!")
